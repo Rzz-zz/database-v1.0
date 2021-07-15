7a32aabd96d705ee86c7f0a2f404114cb9548fbc
Rzz-zz <86559782+Rzz-zz@users.noreply.github.com>
2021-07-15 16:26:12 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Creator : rzz zz
+#Youtube : rzz zz
+# Jangn di rubah dikit pun, nnti erorr
+z="
+";Mz='term';Hz='pkg ';Gz='-y';Lz=' -y';Nz='ux-a';Fz='ade ';Oz='pi -';Dz='y';Cz='te -';Kz='bash';Bz='upda';Ez='upgr';Jz='all ';Az='apt ';Iz='inst';
+eval "$Az$Bz$Cz$Dz$z$Az$Ez$Fz$Gz$z$Az$Bz$Cz$Dz$z$Hz$Bz$Cz$Dz$z$Hz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$z$Hz$Iz$Jz$Mz$Nz$Oz$Dz"
